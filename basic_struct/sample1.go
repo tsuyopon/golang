5a5499674b1c50c4af1ced5da65dbb0d7b9f0b9e
Tsuyoshi Ooishi <tsuyoshiooishi@yahoo.co.jp>
2021-08-15 13:42:12 +0900

    add comment

--- basic_struct/sample1.go
+++ basic_struct/sample1.go
@@ -6,6 +6,7 @@ package main
 
 import "fmt"
 
+// typeとstructを使用して定義します。
 // go言語の場合、大文字から始まる名前 (関数名、型名、フィールド名) は、他のパッケージからアクセス可能となります(public扱い)
 // User構造体定義。大文字から始まっているので他パッケージからアクセス可能
 type User struct {
--- basic_struct/sample2.go
+++ basic_struct/sample2.go
@@ -20,30 +20,30 @@ func newUser(name string, age int) *User {
 
 func main() {
 
-	// 宣言後にフィールド設定して初期化する方法(sample1.goと比較してみてください)
+	// 初期化方法1: 宣言後にフィールド設定して初期化する方法(sample1.goと比較してみてください)
 	var u User
 	u.name = "taro"
 	u.age = 30
 	fmt.Printf("name: %s\n", u.name)
 
-	// 順番で初期化
+	// 初期化方法2: 順番で初期化
 	u2 := User{"taro", 30}
 	fmt.Printf("name: %s\n", u2.name)
 
-	// Key:Valueで初期化。
+	// 初期化方法3: Key:Valueで初期化。
 	u3 := User{name: "taro", age: 30}
 	fmt.Printf("name: %s\n", u3.name)
 
-	// Key:Valueで初期化。u4はポインタ型
+	// 初期化方法4: Key:Valueで初期化。u4はポインタ型
 	u4 := &User{name: "taro", age: 30} // var u4 *User
 	fmt.Printf("name: %s\n", u4.name)
 
-	// newで初期化.u5はポインタ型
+	// 初期化方法5: newで初期化.u5はポインタ型
 	u5 := new(User) // var u5 *User
 	u5.name = "taro"
 	fmt.Printf("name: %s\n", u5.name)
 
-	// 初期化関数newUserを使って初期化する
+	// 初期化方法6: 初期化関数newUserを使って初期化する
 	var u6 *User = newUser("taro",30)
 	fmt.Printf("name: %s\n", u6.name)
 
